85bcbe24509f1431d9d141e62b81416c0ac56444
JoshuaAv07 <a6520150001@utch.edu.mx>
2022-05-17 13:38:43 -0600

    end points changes (GET) and devices

--- server.py
+++ server.py
@@ -1,33 +1,28 @@
 from flask import Flask, request
 
 app = Flask('__main__')
-user = {
-    "matricula":"112233",
-    "first_name":"SOLOMIRO",
-    "last_name":"PEGAMENTOSOS",
-    "phone":"614-1112233"
+
+device = {
+    "code": "12312414",
+    "descrip": "Temp, sensor",
+    "value": 67
 }
-# GET methods
 
-#get users
-@app.route('/users', methods=['GET'])
-def get_users():
-    return user
-
-# POST methods
-
-# save an user
+@app.route('/devices', methods=['GET'])
+def go_home():
+    print(device)
+    return device
+    
+    #Save an user
 @app.route('/users', methods=['POST'])
-def save_user():
+def save_users():
     user = request.json
     print(user)
     return user
-
-#save a device
+    
 @app.route('/devices', methods=['POST'])
 def save_device():
     device = request.json
-    print(device)
     return device
 
 if __name__ == '__main__':
