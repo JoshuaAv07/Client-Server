160298aa3a05abed632641238109f3494ff817d4
JoshuaAv07 <a6520150001@utch.edu.mx>
2022-06-02 11:40:14 -0600

    def save_sensor

--- server.py
+++ server.py
@@ -32,18 +32,18 @@ def save_device():
     print(device)
     return device, 201
 
-@app.route('/sensor', methods['POST'])
-    def save_sensor:
-        device = request.json
-        print(device)
-        ''' Pythonic form '''
-        with open('devices.dat', 'a') as f:
-            f.write(str(device +'\n'))
-            ''' For normal languages
-            f = open('devices.dat', 'a')
-            f.write(str(device))
-            f.close()
-            '''
+@app.route('/sensor', methods=['POST'])
+def save_sensor():
+    device = request.json
+    print(device)
+    ''' Pythonic form '''
+    with open('devices.dat', 'a') as f:
+        f.write(str(device +'\n'))
+        ''' For normal languages
+        f = open('devices.dat', 'a')
+        f.write(str(device))
+        f.close()
+        '''
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
